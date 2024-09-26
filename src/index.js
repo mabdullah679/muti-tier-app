7addfa5d5e6e6a15877701a7ccb7ad041426618e
Muhammad Abdullah <muhammad.abdullah.0913@gmail.com>
2024-09-26 18:11:38 -0400

    Fix Dockerfile path and commit workflow changes

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function App() {
+  return <h1>Hello, Multi-Tier App!</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
